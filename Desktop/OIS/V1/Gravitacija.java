072d192bdff31e762950c3e78b3b0e85926c2ad9
JanaVolk <jv0719@student.uni-lj.si>
2021-04-10 10:24:08 +0200

    Ne upoštevaj sprememb v podatkovni bazi

--- Desktop/OIS/V1/Gravitacija.java
+++ Desktop/OIS/V1/Gravitacija.java
@@ -1,5 +1,24 @@
+import java.util.*;
+
 public class Gravitacija{
+	
     public static void main(String[]args){
-        System.out.println("OIS je zakon!");
+
+		izracun();
+    } 
+	public static void izracun(){
+		Scanner sc = new Scanner(System.in); 
+		
+		double v = sc.nextDouble(); 
+		double c = 6.674*(Math.pow(10,-11));
+		double m = 5.972*(Math.pow(10,24));
+        double r = 6.371*(Math.pow(10,6));
+		double a = (c*m)/((r+v)*(r+v));
+		izpis(v, a);
+	}
+
+    public static void izpis(double visina, double pospesek){
+        System.out.printf("Podana nadmorska višina je %f, podan gravitacijski pospešek je %f", visina, pospesek);
     }
+
 }
